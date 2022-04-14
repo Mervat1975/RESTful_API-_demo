df28470ebfd8168c65fb3c577e49c55dcb98794d
Mervat1975 <mervat.mustafa@dcmail.ca>
2022-04-13 21:51:51 -0400

    typeo

--- student_app/models.py
+++ student_app/models.py
@@ -4,7 +4,6 @@ from django.db import models
 
 
 class Student(models.Model):
-    DISCOUNT_RATE = 0.10
 
     id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=200)
